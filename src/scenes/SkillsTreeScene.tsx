7732b009924f69f4fcfa290959fa052664493a73
Edgar Onghena <46636609+edgarogh@users.noreply.github.com>
2020-04-18 22:10:22 +0200

    :sparkles: Begin implementing SkillsTreeScene

--- src/scenes/SkillsTreeScene.tsx
+++ src/scenes/SkillsTreeScene.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import SkillTree from '../components/SkillTree';
+import skills from '../data/skills.json';
 
 export default function SkillsTreeScene() {
     return (
-        <div>TODO: SkillsTree</div>
-    );
+        <div>
+            <SkillTree skills={skills} />
+        </div>
+    )
 }
