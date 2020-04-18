a9015506d7fe7225c5922e103945d8a16d9ba7e0
Edgar Onghena <46636609+edgarogh@users.noreply.github.com>
2020-04-18 11:16:07 +0200

    :ambulance: Fix stupid error
    Commiting without testing is bad kids.
    
    jk, testing is doubting

--- src/script.ts
+++ src/script.ts
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
 
-ReactDOM.render(React.createElement(Game), document.createElement('#app'));
+ReactDOM.render(React.createElement(Game), document.querySelector('#app'));
