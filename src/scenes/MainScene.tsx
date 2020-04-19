32dd7461327bb2d0d12510326170b90cb3ac2fa2
Ana Gelez <ana@gelez.xyz>
2020-04-19 13:28:55 +0200

    :lipstick: Add icons to the game interface

--- src/scenes/MainScene.tsx
+++ src/scenes/MainScene.tsx
@@ -55,7 +55,7 @@ function Gauge(props = { id: 'hunger', value: 0.5 }) {
     const size = props.value * 100;
     return (
         <div className={"gauge " + props.id}>
-            <img src="todo" alt={props.id} />
+            <img src={`/icons/gauges/${props.id}.png`} alt={props.id} />
             <main>
                 <div className="fill" style={{ width: `${size}%`}}></div>
             </main>
