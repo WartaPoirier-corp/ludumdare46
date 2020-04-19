f61b9abba66c2eeb01558ac6b85832c3cb4b85bd
Edgar Onghena <46636609+edgarogh@users.noreply.github.com>
2020-04-19 19:09:00 +0200

    :sparkles: Make unlocking skills reroute to main

--- src/scenes/SkillsTreeScene.tsx
+++ src/scenes/SkillsTreeScene.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SkillTree from '../components/SkillTree';
 import skills from '../data/skills.json';
+import { goTo } from '../store/actions/router';
 import { unlockSkill } from '../store/actions/skills';
 import { State } from '../store/reducers';
 
@@ -11,6 +12,7 @@ export default function SkillsTreeScene() {
 
     const onUserUnlockSkill = useCallback((skillId: string) => {
         dispatch(unlockSkill(skillId));
+        dispatch(goTo('main-scene'));
     }, [dispatch]);
 
     return (
