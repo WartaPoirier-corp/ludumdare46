44acba9a20b63da502ba9e1c83a92883fa713a61
Ana Gelez <ana@gelez.xyz>
2020-04-20 20:34:35 +0200

    :zap: Preload assets

--- src/Game.tsx
+++ src/Game.tsx
@@ -9,9 +9,67 @@ import MainScene from './scenes/MainScene';
 import Menu from './scenes/MenuScene';
 import { default as store, State } from './store';
 
+const imgs = [
+    'animals/parasite.png',
+    'animals/para-otter.png',
+    'animals/para-cat.png',
+    'animals/dog.png',
+    'animals/para-dog.png',
+    'animals/triceratops.png',
+    'animals/cat.png',
+    'animals/otter.png',
+    'game_over.png',
+    'icons/tentacle1.png',
+    'icons/gauges/libido.png',
+    'icons/gauges/hunger.png',
+    'icons/gauges/health.png',
+    'icons/gauges/mood.png',
+    'icons/gauges/energy.png',
+    'icons/gauges/peepoo.png',
+    'icons/gauges/suspicion.png',
+    'icons/music-off.png',
+    'icons/favicon.png',
+    'icons/music-on.png',
+    'icons/tentacle2.png',
+    'icons/sound-off.png',
+    'icons/menu.png',
+    'icons/tentacle3.png',
+    'icons/tentacle4.png',
+    'icons/sound-on.png',
+    'icons/title.png',
+    'skills/feelings-1.png',
+    'skills/root.png',
+    'skills/vision-3.png',
+    'skills/feelings-2.png',
+    'skills/smell-2.png',
+    'skills/sound-1.png',
+    'skills/vision-2.png',
+    'skills/needs-1.png',
+    'skills/sound-2.png',
+    'skills/needs-2.png',
+    'skills/needs-3.png',
+    'skills/smell-3.png',
+    'skills/smell-1.png',
+    'skills/feelings-3.png',
+    'skills/vision-1.png',
+    'skills/sound-3.png',
+];
+const sounds = [
+    '/audio/gnuh.mp3',
+    '/audio/click.mp3',
+    '/audio/cat.mp3',
+    '/audio/dog.mp3',
+    '/audio/otter.mp3',
+    '/audio/triceratops.mp3',
+];
+
 export default function Game() {
     return (
         <ReduxProvider store={store}>
+            <div class="invisible">
+                {imgs.map(i => <img src={i} />)}
+                {sounds.map(s => <audio src={s}></audio>)}
+            </div>
             <Router />
         </ReduxProvider>
     );
