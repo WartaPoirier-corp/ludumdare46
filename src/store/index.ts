22034a3731a1820012ba1115fc4608a95d72a4c1
Ana Gelez <ana@gelez.xyz>
2020-04-20 19:32:22 +0200

    :ambulance: Remove redux dev tools they make everything crash

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,5 +8,5 @@ export default createStore(
     reducers, // Initialization reducer
     initialState,
     // @ts-ignore
-    compose(persistState(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(persistState())
 );
